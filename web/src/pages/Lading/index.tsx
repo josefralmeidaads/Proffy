d32495975afb04e7d3a39dbec2594b12212e4c38
Jose Francisco de Almeida Filho <69639482+josefralmeidaads@users.noreply.github.com>
2020-08-26 10:53:10 -0300

    web

--- web/src/pages/Lading/index.tsx
+++ web/src/pages/Lading/index.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './styles.css'
 
 import logoImg from '../../assets/images/logo.svg'
+import landingImg from '../../assets/images/landing.svg'
+
+import studyIcon from '../../assets/images/icons/study.svg'
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 const Landing = () => {
     return(
@@ -11,6 +16,22 @@ const Landing = () => {
                     <img src={logoImg} alt="Proffy" />
                     <h2>Sua Plataforma de Estudos Online!</h2>
                 </div>
+                <img src={landingImg} alt="landingImage" className="hero-image" />
+                
+                <div className="buttons-container">
+                    <a href="" className="study">
+                        <img src={studyIcon} alt="study" />
+                        Estudar
+                    </a>
+                    <a href="" className="give-classes">
+                        <img src={giveClassesIcon} alt="give-classes" />
+                        Dar aulas
+                    </a>
+                </div>
+             
+              <span className="total-connections">
+                  Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+              </span>
             </div>
         </div>
     )
